073685ac1c4b68448753fdaadd11a63171458109
Buongiorno <mnbuongiorno@gmail.com>
2024-03-25 16:53:51 -0300

    Created tasks api calls updated tasks pages routed new pages

--- client/src/pages/TasksFormPage.jsx
+++ client/src/pages/TasksFormPage.jsx
@@ -1,9 +1,55 @@
+import { useForm } from "react-hook-form";
+import { createTask, deleteTask } from "../api/tasks.api";
+import { useNavigate, useParams } from "react-router-dom";
+
 export function TasksFormPage() {
-    return (
-      <div>
-        Task form page
-      </div>
-    )
-  }
-  
-  
+  const { register, handleSubmit, formState: {
+    errors
+  } } = useForm();
+  const navigate = useNavigate();
+  const params = useParams();
+  console.log(params)
+
+  const onSubmit = handleSubmit(async data => {
+    const res = await createTask(data)
+    navigate("/tasks");
+    console.log(res)
+
+  })
+
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Title"
+          {...register("title", { required: true })}
+        />
+        {errors.title && <span>Title required </span>}
+
+        <textarea
+          rows="3"
+          placeholder="Description"
+          {...register("description", { required: true })}
+        ></textarea>
+        {errors.description && <span>Description required </span>}
+        <button>Save</button>
+      </form>
+
+    {
+      params.id && (
+        <button onClick={async ()=>{
+          const accepted = window.confirm('are you sure?')
+          if (accepted){
+            deleteTask(params.id);
+            navigate('/tasks');
+          }
+        }}>
+        Delete 
+      </button>
+      )
+    }
+    </div>
+  );
+}
